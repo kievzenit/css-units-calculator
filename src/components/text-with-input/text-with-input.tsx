ed644b1b5732759cbe24938f5f8ae13cca963503
kievzenit <contact@kievzenit.tk>
2023-06-02 18:47:37 +0300

    TextWithInput component created

---
+++ src/components/text-with-input/text-with-input.tsx
@@ -0,0 +1,22 @@
+import { QRL, Signal, component$ } from "@builder.io/qwik";
+
+import CustomInput from "../custom-input/custom-input";
+
+import styles from "./text-with-input.module.scss";
+
+interface TextWithInputProps {
+    cssUnitName: string;
+    value: Signal<string>;
+    calculateResults$?: Array<QRL<(value: number) => number>>;
+    setResults$?: Array<QRL<(result: number) => void>>;
+}
+
+export default component$<TextWithInputProps>((props) => {
+    return (
+        <div class={styles.container}>
+            <p class={styles.text}>{props.cssUnitName}:</p>
+            <CustomInput value={props.value} placeholder={props.cssUnitName}
+                calculateResults$={props.calculateResults$} setResults$={props.setResults$} />
+        </div>
+    );
+})
