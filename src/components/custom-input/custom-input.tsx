5e051af64aa02d2693cab74a255da2a558d08ea5
kievzenit <contact@kievzenit.tk>
2023-06-02 18:47:10 +0300

    CustomInput component created

---
+++ src/components/custom-input/custom-input.tsx
@@ -0,0 +1,27 @@
+import { component$, QRL, Signal, } from "@builder.io/qwik";
+
+import styles from "./custom-input.module.scss";
+
+interface InputProps {
+    value: Signal<string>;
+    placeholder?: string;
+    calculateResults$?: Array<QRL<(value: number) => number>>;
+    setResults$?: Array<QRL<(result: number) => void>>;
+}
+
+export default component$<InputProps>((props) => {
+    return <input class={styles.input} type="text" bind:value={props.value} placeholder={props.placeholder}
+        onInput$={async (e) => {
+            if (!props.calculateResults$ || !props.setResults$) {
+                return;
+            }
+
+            props.calculateResults$.forEach(async (calculateResult, index) => {
+                const result = await calculateResult(e.target!.value);
+
+                const setResult = props.setResults$![index];
+
+                await setResult(result);
+            });
+        }} />;
+});
